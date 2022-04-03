c636c5ef6d5fd32be8a60afdb0e7bb3cd6b531cd
Ranjan <skillsapphire@gmail.com>
2022-04-03 14:52:13 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import About from './screens/About';
+import Home from './screens/Home';
+import Contact from './screens/Contact';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavBar></NavBar>
+        <Routes>
+          <Route exact path='/' element={<Home />}></Route>
+          <Route exact path='/about' element={<About />}></Route>
+          <Route exact path='/contact' element={<Contact />}></Route>
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
